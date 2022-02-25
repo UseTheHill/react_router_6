cdcbadc5a1f160016602933c6cae8c84f48d5d6d
Kayte <kaytemcdonough@gmail.com>
2022-02-25 13:47:08 -0500

    navbar work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container } from "react-bootstrap/";
 
 // Pages
 import Home from "./pages/Home";
@@ -10,12 +12,31 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <nav>
+        <Navbar className="km_nav_bar" expand="lg">
+          <Container className="km_nav_container">
+            <Navbar className="km_nav_bar_brand" href="/">Kayte McDonough</Navbar>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="km_nav_collapse"id="basic-navbar-nav">
+              <Nav className="me-auto km_nav_link_group">
+                <Link className="km_nav_link" to="/">
+                  Home
+                </Link>
+                <Link className="km_nav_link" to="/projects">
+                  Projects
+                </Link>
+                <Link className="km_nav_link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        {/* <nav>
           <h1>Kayte McDonough</h1>
           <Link to="/">Home</Link>
           <Link to="/projects">Projects</Link>
           <Link to="/contact">Contact</Link>
-        </nav>
+        </nav> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
